b160e23002338f4cd5b6a60117d8c94ba745331c
amandavo <amandavoliveira0@gmail.com>
2023-06-16 12:14:35 -0300

    adaptando arquivos e adicionando maquinas

--- automanager/src/main/java/com/autobots/automanager/dto/LoginDto.java
+++
@@ -1,5 +0,0 @@
-package com.autobots.automanager.dto;
-
-public record LoginDto(String login, String password) {
-	
-}
--- automanager/src/main/java/com/autobots/automanager/enumeracoes/PerfilUsuario.java
+++
@@ -1,5 +0,0 @@
-package com.autobots.automanager.enumeracoes;
-
-public enum PerfilUsuario {
-	ROLE_ADMIN, ROLE_CLIENTE, ROLE_GERENTE, ROLE_VENDEDOR
-}
